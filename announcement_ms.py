64b55ac4973dbbedc0ee93cff58aa6120b93e0dd
Ahmed Yasin KUL <ahmedyasinkul@hotmail.com>
2018-12-25 15:46:42 +0300

    Removed unnecessary lines from code

--- announcement_ms.py
+++ announcement_ms.py
@@ -1,28 +1,24 @@
 from flask import jsonify, request, abort
-from flask_httpauth import HTTPBasicAuth
 from announcement_config import app
 from announcement_db import add_announcement, get_announcements
 
 
-auth = HTTPBasicAuth()
 
 @app.errorhandler(400)
-def bad_request(error):
+def bad_request(err):
     return jsonify({'error': 'Your request doesn\'t contain JSON'}), 400
 
-@auth.error_handler
-def unauthorized():
+@app.errorhandler(401)
+def unauthorized(err):
     return jsonify({'error': 'Unauthorized access'}), 403
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(err):
     return jsonify({'error': 'Not found'}), 404
 
 
 
-
 @app.route('/announcement/create', methods=['POST'])
-#@auth.login_required
 def create_announcement():
     if not request.json:
         abort(400)
@@ -37,8 +33,8 @@ def create_announcement():
 
     return jsonify({'result': 'Success', 'announcement_id':new_announcement.announcement_id}), 200
 
+
 @app.route('/announcement/get', methods=['GET'])
-#@auth.login_required
 def get():
     announcements = get_announcements()
     if announcements is None:
@@ -48,13 +44,6 @@ def get():
         announcement_list.append({"announcement_id":i.announcement_id, "title":i.title, "text":i.text, "image_link":i.image_link, "movie_link":i.movie_link})
     return jsonify({'result': 'Success', 'announcement_list':announcement_list}), 200
 
-
-
-# Validate the admin signin#######TODO###############
-@auth.verify_password
-def verify_password(username, password):
-    # TODO: Change check if is admin in the database or not.
-    return username == 'admin' and password == 'asdqwe123'
 
 
 if __name__ == '__main__':
